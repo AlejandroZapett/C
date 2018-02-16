36e081a6b4a3a00435aaa353c7bd436894414eff
AlejandroZapett <gunsn_1rosesslash@hotmail.com>
2018-02-16 14:42:28 -0600

    Agregando una linea a hola.c desde develop

--- hola.c
+++ hola.c
@@ -3,8 +3,8 @@
 int main(){
 	print("Hola mundo cruel y despiadado\n");
 	printf(":)\n");
-    printf(":(\n");
-
+	printf(":(\n");
+	puts("Modifiqué esta ultima linea en la rama develop\n");
 	return 0;
 }
 
